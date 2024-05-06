bca2d094c97ae943331558c6f9ea2d30a20e7050
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-05-06 15:45:23 +0200

    feat: AppHeader completed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  
+  <AppHeader />
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-  
+  components: { 
+    AppHeader 
+  },
+
 }
 </script>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-auto mt-3">
+                <h1>Rick and Morty App</h1>
+            </div>
+        </div>
+        
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
